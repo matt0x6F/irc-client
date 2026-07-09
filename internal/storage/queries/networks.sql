3220eabb4e67a7296f807e36c82d6ae10059875e
Matt Ouille <matt.ouille@protonmail.com>
2026-07-09 09:06:47 -0700

    feat(storage): sqlc queries for network color/icon/sort_order

--- internal/storage/queries/networks.sql
+++ internal/storage/queries/networks.sql
@@ -2,7 +2,7 @@
 SELECT * FROM networks WHERE id = ?;
 
 -- name: GetNetworks :many
-SELECT * FROM networks ORDER BY name;
+SELECT * FROM networks ORDER BY sort_order, id;
 
 -- name: CreateNetwork :one
 INSERT INTO networks (name, address, port, tls, nickname, username, realname, password, sasl_enabled, sasl_mechanism, sasl_username, sasl_password, sasl_external_cert, auto_connect, identify_as_bot, created_at, updated_at)
@@ -20,6 +20,18 @@ WHERE id = ?;
 
 -- name: UpdateNetworkAutoConnect :exec
 UPDATE networks SET auto_connect = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?;
+
+-- name: UpdateNetworkColor :exec
+UPDATE networks SET color = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?;
+
+-- name: UpdateNetworkIcon :exec
+UPDATE networks SET icon_path = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?;
+
+-- name: ClearNetworkIcon :exec
+UPDATE networks SET icon_path = NULL, updated_at = CURRENT_TIMESTAMP WHERE id = ?;
+
+-- name: UpdateNetworkSortOrder :exec
+UPDATE networks SET sort_order = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?;
 
 -- name: DeleteNetwork :exec
 DELETE FROM networks WHERE id = ?;
